880572cb43b8c9a7985b17511d794f2a9250ae2a
Toni Domazet <toni.domaz93@hotmail.com>
2018-08-30 21:52:58 +0100

    starting the burger component burger ingrediens func.comp to render ingredient based on prop type

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
-import './App.css';
-
+import Layout from "./components/Layout/Layout";
+import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder"
 class App extends Component {
   render() {
     return (
-      <div>
-
-      </div>
+      <Layout>
+        <BurgerBuilder />
+      </Layout>
     );
   }
 }
 
-export default App;
+export default App; 
+  
