c5dc92a22b651d7442100880ef25638ab0207206
Toni Domazet <toni.domaz93@hotmail.com>
2018-09-01 12:28:15 +0100

    burger ingredients - adding PropTypes check ingredients - if empty show message

---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,28 @@
+import React from "react";
+import classes from "./Burger.css";
+import BurgerIngredients from "./BurgerIngredients/BurgerIngredients";
+
+const burger = (props) => {
+
+  let transformedIngredients = Object.keys(props.ingrediens)
+  .map((ingr) => {
+    return [...Array(props.ingrediens[ingr])].map((_, i) => {
+      return <BurgerIngredients key={ingr + 1 } type={ingr} />
+    })
+  }).reduce((arr, el) => [...arr, ...el] ,[]);
+
+  if(!transformedIngredients.length) {
+    transformedIngredients = <p>Please start adding ingrediens</p>
+  }
+
+  return (
+    <div className={classes.Burger}>
+      <BurgerIngredients type="bread-top"/>
+        {transformedIngredients} 
+      <BurgerIngredients type="bread-bottom"/>
+
+    </div>
+  )
+}
+
+export default burger;
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,16 +1,24 @@
 import React, { Component } from "react";
 import Aux from "../../hoc/Aux";
-
+import Burger from  "../../components/Burger/Burger"
 class BurgerBuilder extends Component {
 
+  state = {
+    ingrediens: {
+      salad: 0,
+      bacon: 0,
+      cheese: 0,
+      meat: 0
+    }
+  }
 
   render() {
       return (
         <Aux>
-          <div>Burger</div>
+          <Burger ingrediens={this.state.ingrediens} />
           <div>Build Controls</div>
         </Aux>
-      )
+      ) 
   }
 }
 
