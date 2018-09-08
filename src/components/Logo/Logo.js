504025f3f9eb0a2de10df2546d495cea79fa4042
Toni Domazet <toni.domaz93@hotmail.com>
2018-09-08 14:37:17 +0100

    adding SideDrawer component toggleHandler to show sidebar

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import burgerLogo from "../../assets/images/burger-logo.png";
-import classed from "./Logo.css"
+import classes from "./Logo.css"
 const logo = (props) => (
-  <div className={classed.Logo}>
+  <div className={classes.Logo} style={{ height: props.height }}>
     <img src={burgerLogo} alt="myBurger" />
   </div>
 )
