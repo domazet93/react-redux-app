d49aa783d10425fc44dc894b1fdbfccb004264de
Toni Domazet <toni.domaz93@hotmail.com>
2018-09-15 18:31:46 +0100

    create hoc - withErrorHandler to show http error

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,38 +1,39 @@
-
 import React, { Component } from "react";
 import Aux from "../Aux/Aux";
-import classes from "./Layout.css"
+import classes from "./Layout.css";
 import Toolbar from "../../components/Navigation/Toolbar/Toolbar";
-import SideDrawer from '../../components/Navigation/SideDrawer/SideDrawer';
+import SideDrawer from "../../components/Navigation/SideDrawer/SideDrawer";
 
 class Layout extends Component {
   state = {
     showSideDrawer: false
-  }
+  };
 
   sideDrawerCloseHandler = () => {
     this.setState({
       showSideDrawer: false
-    })
-  }
+    });
+  };
 
   sideDrawerToggleHandler = () => {
-    this.setState((prevState) => {
+    this.setState(prevState => {
       return {
         showSideDrawer: !prevState.showSideDrawe
-      }
-    })
-  }
+      };
+    });
+  };
 
-  render() {    
+  render() {
     return (
-      <Aux> 
-        <Toolbar drawerToggleHandler={this.sideDrawerToggleHandler}/>
-        <SideDrawer open={this.state.showSideDrawer} closed={this.sideDrawerCloseHandler}/>
+      <Aux>
+        <Toolbar drawerToggleHandler={this.sideDrawerToggleHandler} />
+        <SideDrawer
+          open={this.state.showSideDrawer}
+          closed={this.sideDrawerCloseHandler}
+        />
         <main className={classes.Content}>{this.props.children}</main>
       </Aux>
-    )
+    );
   }
-  
 }
 export default Layout;
